42c3e336b28c54ab7071d571fe62df2dc9f3abfb
adamarisBoot <adaparedessolano@crackthecode.la>
2024-08-22 17:35:48 -0500

    first commit

---
+++ assets/formulario.js
@@ -0,0 +1,16 @@
+function genero(){
+    const form = document.getElementById('access-from');
+    
+    
+        form.addEventListener('submit',(e) =>{
+        e.preventDefault();
+        const gender = document.querySelector('input[name="gender"]:checked').value;
+        if (gender === 'female') {
+            window.location.href = '../html/inicio.html';
+        } else if (gender === 'male') {
+            window.location.href = '../html/men-only.html';
+            
+        }
+    }); 
+    }
+    genero()
